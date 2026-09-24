6ea18bcaf4fa0d53c0ecd45536e6be351e3392c0
VijayKumar <148474827+vijaykumarbvk@users.noreply.github.com>
2026-09-24 21:30:05 +0530

    Change S3 bucket name to a unique identifier
    Updated S3 bucket name for Terraform backend.

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "venus-hospital-tfstate-vkb"   # <-- CHANGE to a globally-unique bucket
+    bucket       = "venus-hospital-tfstate-vj"   # <-- CHANGE to a globally-unique bucket
     key          = "venus-hospital/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
